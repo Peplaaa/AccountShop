2f455775d05c221caac601d80914b8b881679c77
Kamil <pepla9966@gmail.com>
2018-05-22 10:40:11 +0200

    init

---
+++ DiagramUML/src/main/java/com/mycompany/diagramuml/shoppingCart.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.diagramuml;
+
+/**
+ *
+ * @author 165NCIPEPLINSKIK
+ */
+public class shoppingCart {
+    public float totalMoney;
+    public float vatAmount;
+    public String listAccount;
+    public void addToCart() {
+        
+    }
+    public void showTotalPrice() {
+        
+    }
+}
